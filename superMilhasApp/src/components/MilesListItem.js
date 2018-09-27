25a11907a3c8970c8319ea1ba9754fe1a5311c40
Joseph Weber <joseph.weber@acad.pucrs.br>
2018-09-26 22:33:39 -0300

    Correção ainda quebrada

--- superMilhasApp/src/components/MilesListItem.js
+++ superMilhasApp/src/components/MilesListItem.js
@@ -6,22 +6,44 @@ import type { MilesAgency } from "../util/types";
 const MilesListItem = ({ milesAgency }: MilesAgency) => {
   return (
     <View style={styles.generalView}>
-      <View style={{ flex: 1 / 2 }}>
+      <View style={{ flex: 1/3 , 
+          borderColor: 'midnightblue', 
+          borderWidth: 1,    
+          borderRadius: 10
+      }}>
         <Image
           style={styles.image}
-          borderRadius={12}
+          borderRadius={20}
+
           source={{ uri: milesAgency.programa_default.imagem }}
         />
       </View>
       <View style={styles.column}>
-        <View
-          style={{ flex: 1, justifyContent: "center", marginLeft: 10 }}>
+        <View style={{ flex: 3, justifyContent: "center", alignItems: "center", marginLeft: 10,
+            borderColor: 'midnightblue', 
+    borderWidth: 1,    
+    borderRadius: 10,
+    width: 150, 
+    height: 45}}>
           <Text style={styles.programName}>{milesAgency.nome}</Text>
-          <Text style={styles.date}>{milesAgency.milha_expiracao_maisProxima}</Text>
+        </View>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", marginLeft: 20 }}>
+          <Text style={styles.date}>
+            {milesAgency.milha_expiracao_maisProxima} 
+            vencem em 
+            {milesAgency.date}
+          </Text>
         </View>
       </View>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={styles.miles}>{milesAgency.somaMilhas}</Text>
+      <View style={{ flex: 1 / 2, justifyContent: "center", alignItems: "center", borderColor: 'midnightblue', 
+    borderWidth: 1,    
+    borderRadius: 10,
+    width: 50, 
+    height: 50 }}>
+        <Text style={styles.miles}>
+          <Text style ={{fontSize: 12}}> total </Text>
+          {milesAgency.somaMilhas}
+        </Text>
       </View>
     </View>
   );
@@ -32,26 +54,34 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 10,
     backgroundColor: "white",
-    marginHorizontal: 10
+    marginHorizontal: 10    
   },
-  image: { 
+  image: {     
     width: 64, 
     height: 64, 
-    margin: 10 
+    margin: 10,
+    resizeMode: 'contain'
   },
+  
   columnView: { 
     flexDirection: "column",
-     flex: 1 
+    flex: 1
   },
   programName: {
     fontSize: 24,
-    textAlign: 'left'
+    textAlign: 'center',
+    margin: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: 'midnightblue'
   },
   date: {
-    textAlign: 'left'
+    fontSize: 10,
+    textAlign: 'center',
+    height: 30
   },
   miles: {
-    fontSize: 30
+    fontSize: 30,
+    textAlign: 'center'
   }
 });
 
